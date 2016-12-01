160d38a6716f2c5f5faa57ab1d0588689e71f0b4
Grace <gcyw@umich.edu>
2016-12-01 16:35:19 -0500

    crawler prints the puns on that page

--- crawler/Gracetry.py
+++ crawler/Gracetry.py
@@ -3,36 +3,6 @@ import re
 import requests
 
 
-def _get_origin(tree):
-	'''
-		Return a dictionary with the year in which the word was first used and
-		a broader description of its origins.
-	'''
-	# Some words use XPATH1 to segway into the origins section, but some use
-	# XPATH2 instead. We will need to account for both.
-	XPATH1 = '//div[@class="etymology"]/div//text()'
-	XPATH2 = '//div[@class="first-use"]/div/text()'
-	DATE_PREFIX = 'First Known Use:' # prefixes date/year in XPATH1
-	
-	initial = tree.xpath(XPATH1)
-	alternate = tree.xpath(XPATH2)
-	# Usual processing
-	origin = [_trim(elem) for elem in initial + alternate if _trim(elem)]
-	origin_results = {'origin_date': set(), 'origin_description': set()}
-
-	for detail in origin:
-		# Date if prefixed by 'First Known Use:' (XPATH1) or if value is
-		# in XPATH2's results
-		if DATE_PREFIX in detail or detail in alternate:
-			origin_results['origin_date'].add(detail.strip(DATE_PREFIX))
-		# If not date, then origin textual description
-		else:
-			origin_results['origin_description'].add(detail)
-
-	return origin_results
-
-
-
 
 
 
@@ -47,6 +17,8 @@ if __name__ == '__main__':
 	htmlpage = htmlpage.split("<!-- Puns -->")[1]
 	htmlpage = htmlpage.split("<!-- google_ad_section")[0]
 
-	# Parse, and assemble results in dict.
 
-	print htmlpage
+	for line in htmlpage.split("\n"):
+		if '<div class="credit"' in line:
+			line2 = line[line.index("<td>") + len("<td>"):line.index("<div class=")]
+			print line2
