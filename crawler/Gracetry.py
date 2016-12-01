d06ae2152f751374ed3475543f8db5057a3981e0
jenningsjin <jinjenni@umich.edu>
2016-12-01 16:45:07 -0500

    crawler work

--- crawler/Gracetry.py
+++ crawler/Gracetry.py
@@ -8,17 +8,22 @@ import requests
 
 if __name__ == '__main__':
 
-	word = '1'
-	url = ''.join(['http://www.punoftheday.com/cgi-bin/disppuns.pl?ord=S&cat=4&sub=0401&page=', word])
-	page = requests.get(url)
-	tree = html.fromstring(page.text)
+	# word = '1'
+	# url = ''.join(['http://www.punoftheday.com/cgi-bin/disppuns.pl?ord=S&cat=4&sub=0401&page=', word])
+	url = 'http://www.punoftheday.com/cgi-bin/disppuns.pl?ord=S&cat=4&sub=0401&page='
+	
 
-	htmlpage = html.tostring(tree)
-	htmlpage = htmlpage.split("<!-- Puns -->")[1]
-	htmlpage = htmlpage.split("<!-- google_ad_section")[0]
+	punList = []
+	for i in xrange(1,10):
+		page = requests.get(url + str(i))
+		tree = html.fromstring(page.text)
+		htmlpage = html.tostring(tree)
+		htmlpage = htmlpage.split("<!-- Puns -->")[1]
+		htmlpage = htmlpage.split("<!-- google_ad_section")[0]
+			
 
-
-	for line in htmlpage.split("\n"):
-		if '<div class="credit"' in line:
-			line2 = line[line.index("<td>") + len("<td>"):line.index("<div class=")]
-			print line2
+		for line in htmlpage.split("\n"):
+			if '<div class="credit"' in line:
+				line2 = line[line.index("<td>") + len("<td>"):line.index("<div class=")]
+				punList.append(line2)
+				print line2
--- crawler/crawler.py
+++ crawler/crawler.py
@@ -11,9 +11,20 @@ if __name__ == "__main__":
 	# Process Seed URL
 	baseLink = baseLink[:-1]
 
+	punList = []
 	#get the first 10 pages of puns
 	for i in xrange(1, 10):
-		page = urllib2.urlopen(baseLink + str(i))
-		print page.read()
-		break
+		htmlpage = urllib2.urlopen(baseLink + str(i))
+		# print page.read()
+		htmlpage = htmlpage.split("<!-- Puns -->")[1]
+		htmlpage = htmlpage.split("<!-- google_ad_section")[0]
+
+
+		for line in htmlpage.split("\n"):
+			if '<div class="credit"' in line:
+				line2 = line[line.index("<td>") + len("<td>"):line.index("<div class=")]
+				print line2
+
+
+
 
