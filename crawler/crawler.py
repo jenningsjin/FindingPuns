5bb0fc7ee7ac63b3e1fda128ee1968ecccaab81f
jenningsjin <jinjenni@umich.edu>
2016-12-01 16:33:06 -0500

    base crawler

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,1 +1,18 @@
 import sys
+import urllib2
+
+seedURLs = ""
+
+if __name__ == "__main__":
+	seedURL = sys.argv[1]
+
+	baseLink = seedURL
+
+	# Process Seed URL
+	baseLink = baseLink[:-1]
+
+	#get the first 10 pages of puns
+	for i in xrange(1, 10):
+		page = urllib2.urlopen(baseLink + i)
+
+
