c42719cce6bc1367ded7300ea9d312c9ce0bda84
Grace <gcyw@umich.edu>
2016-12-04 18:43:59 -0500

    baseline of taking the last word of the sentence

--- Findpuns.py
+++ Findpuns.py
@@ -7,9 +7,41 @@ import sys
 
 class FindPuns:
 	def __init__(self, filename):
-		print filename
+		self.sentenceindex(filename)
 
+	def sentenceindex(self, filename):
+		tri = 0
+		with open(filename) as f:
+			sentence = []
+			word = ""
+			correct = 0
+			total = 0
+			for line in f:
+				if tri == 0:
+					sentence = self.processline(line.split(" "))
+				elif tri == 1:
+					if line == sentence[len(sentence)-1]:
+						correct += 1
+					total += 1
+				elif tri == 2:
+					tri = -1
+				tri += 1
 
+			print "Total: " + str(total)
+			print "Correct: " + str(correct)
+			print "Percentage accuracy: " + str(float(correct)/total)
+
+	def processline(self, sentencelist):
+		sentencereturn = []
+		for word in sentencelist:
+			sentencereturn.append(self.process(word))
+		return sentencereturn
+
+	def process(self, word):
+		for char in word:
+			if char in "\"?!.(),:;'":
+				word = word.replace(char, '')
+		return word.lower()
 
 if __name__ == "__main__":
 	trainfile = sys.argv[1]
