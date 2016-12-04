fdd753bc15ab12463f77ef46dd44b3b61607d3c5
Grace <gcyw@umich.edu>
2016-12-04 18:22:02 -0500

    add stopwords, outline of file

---
+++ Findpuns.py
@@ -0,0 +1,16 @@
+# Team: Radata
+# 	Jennings Jin
+# 	Justin Kim
+# 	Ian Lin
+# 	Grace Wu
+import sys
+
+class FindPuns:
+	def __init__(self, filename):
+		print filename
+
+
+
+if __name__ == "__main__":
+	trainfile = sys.argv[1]
+	FindPuns(trainfile)
