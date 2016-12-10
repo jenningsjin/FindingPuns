a14a16b14b7f2bb4a5f8d824734a14053f977111
Grace <gcyw@umich.edu>
2016-12-10 18:10:03 -0500

    includes stopwords

--- Findpuns.py
+++ Findpuns.py
@@ -4,39 +4,56 @@
 # 	Ian Lin
 # 	Grace Wu
 import sys
+from nltk.corpus import wordnet as wn
 
 class FindPuns:
-	def __init__(self, filename):
-		self.sentenceindex(filename)
+	def __init__(self, filename):		
+		self.correct = []
+		self.guesses = []
+		self.stopwords = ['a','all','an','and','any','are','as','at','be','been','but','by ','few','from','for','have','he','her','here','him','his','how','i','in','is','it','its','many','me','my','none','of','on ','or','our','she','some','the','their','them','there','they','that ','this','to','us','was','what','when','where','which','who','why','will','with','you','your']
 
-	def sentenceindex(self, filename):
-		tri = 0
-		with open(filename) as f:
-			sentence = []
-			word = ""
-			correct = 0
-			total = 0
-			for line in f:
-				if tri == 0:
-					line = line[:len(line)-1]
-					sentence = self.processline(line.split(" "))
-				elif tri == 1:
-					line = line[:len(line)-1]
-					if line == sentence[len(sentence)-1]:
-						correct += 1
-					total += 1
-				elif tri == 2:
-					tri = -1
-				tri += 1
+		# self.sentenceindex(filename)
 
-			print "Total: " + str(total)
-			print "Correct: " + str(correct)
-			print "Percentage accuracy: " + str(float(correct)/total)
+		self.wordnet(filename)
+
+
+
+
+	def wordnet(filename):
+		print "something works"		
+
+
+
+
+	# def sentenceindex(self, filename):
+	# 	tri = 0
+	# 	with open(filename) as f:
+	# 		sentence = []
+	# 		word = ""
+	# 		correct = 0
+	# 		total = 0
+	# 		for line in f:
+	# 			if tri == 0:
+	# 				line = line[:len(line)-1]
+	# 				sentence = self.processline(line.split(" "))
+	# 			elif tri == 1:
+	# 				line = line[:len(line)-1]
+	# 				if line == sentence[len(sentence)-1]:
+	# 					correct += 1
+	# 				total += 1
+	# 			elif tri == 2:
+	# 				tri = -1
+	# 			tri += 1
+
+	# 		print "Total: " + str(total)
+	# 		print "Correct: " + str(correct)
+	# 		print "Percentage accuracy: " + str(float(correct)/total)
 
 	def processline(self, sentencelist):
 		sentencereturn = []
 		for word in sentencelist:
-			sentencereturn.append(self.process(word))
+			if word not in self.stopwords:
+				sentencereturn.append(self.process(word))
 		return sentencereturn
 
 	def process(self, word):
