26a4424d1e3d97b8cb01d9d2567a0d898fc38d95
Ian Lin <ian.lin123@gmail.com>
2016-12-11 17:38:53 -0500

    sentence index cix and simscores update

--- source/simScores.py
+++ source/simScores.py
@@ -4,73 +4,125 @@ import re
 
 def normalize(weights, tot):
 	normweights = []
+	if tot == 0:
+		return weights
+
 	for num in weights:
 		normweights.append(float(num)/tot)
 	return normweights
 
-def simweights(tokens):
+# given a list of scores for every sense of a word
+# returns the probability/weight that the word in reference
+# is a pun word
+def analyzesenses(senses):
+	# must have at least two senses
+	length = len(senses)
+	if length < 2:
+		return 0
+	if length == 2:
+		return max(senses[0], senses[1]) - abs(senses[0] - senses[1])
+
+	one = 0
+	two = 0
+	three = 0
+
+	for s in senses:
+		if s > one:
+			three = two
+			two = one
+			one = s 
+		elif s > two:
+			three = two
+			two = s
+		elif s > three:
+			three = s
+
+	return float((one - three) + (two - three))/2
+
+def simScores(line):
+	linelen = len(line)
+	tot = 0
 	tokens = []
-	for word in line:
-		# only consider tokens that have more than two senses
-		if len(wordnet.synsets(word)) >= 2:
-			tokens.append(word)
+	senses = {}
+	sensesum = {}
+	for token in line:
+		senses[token] = wordnet.synsets(token)
 
 	wordweights = []
+	for token in line:
+		tokensenses = []
+		senseweight = 0
+
+		for ss in senses[token]:
+
+			for othertoken in line:
+				if token != othertoken:
+					for otherss in senses[othertoken]:
+						senseweight += max(0, ss.path_similarity(otherss))
+
+			tokensenses.append(senseweight)
+			senseweight = 0
+
+		weight = analyzesenses(tokensenses)
+		wordweights.append(analyzesenses(tokensenses))
+						# print token + ", " + othertoken
+						# print max(0, ss.path_similarity(otherss))
+						
+
+		# if len(senses[token]) > 0:
+		# 	weight = float(weight)/len(senses[token])
+		# wordweights.append(weight)
+		tot += weight
+
+	return normalize(wordweights, tot)
 
 
 
 
-def simscores(sentences):
-	allweights = []
+# if __name__ == '__main__':
+# 	sents = [ ['rocky', 'rough', 'rock'],
+# 			  ['horse', 'stable', 'animal'],
+# 			  ['deer', 'buck', 'teeth'],
+# 			  ['duck', 'bartender', 'bill']
+# 			]
 
-	for line in sentences:
-		wordweights = []
-		linelen = len(line)
-		tot = 0
-
-		wordweights = simweights(line)
-
-
-
+# 	for line in sents:
+# 		print line
+# 		print simscores(line)
 
 
 
-synsets = {}
-tokenTopScores = {}
-for token in tokens:
-	# get the list of senses of that word
-    synsets[token] = wordnet.synsets(token)
-    tokenTopScores[token] = {}
+# synsets = {}
+# tokenTopScores = {}
+# for token in tokens:
+# 	# get the list of senses of that word
+#     synsets[token] = wordnet.synsets(token)
+#     tokenTopScores[token] = {}
 
-# for every word in that sentence
-for token in tokens:
-	# for every sense for that word
-    for synset in synsets[token]:
+# # for every word in that sentence
+# for token in tokens:
+# 	# for every sense for that word
+#     for synset in synsets[token]:
 
-        tokenTopScores[token][synset] = 0
+#         tokenTopScores[token][synset] = 0
 
-        #for the rest of the words in that sentence
-        for otherToken in tokens:
-        	# skip if same word
-            if token == otherToken:
-                continue
-            maxScore = 0
+#         #for the rest of the words in that sentence
+#         for otherToken in tokens:
+#         	# skip if same word
+#             if token == otherToken:
+#                 continue
+#             maxScore = 0
 
-            # finds the most 
-            for otherSynset in synsets[otherToken]:
-                maxScore = max(maxScore, synset.path_similarity(otherSynset))
-            tokenTopScores[token][synset] += maxScore
+#             # finds the most 
+#             for otherSynset in synsets[otherToken]:
+#                 maxScore = max(maxScore, synset.path_similarity(otherSynset))
+#             tokenTopScores[token][synset] += maxScore
 
-for token in tokens:
-    print(token)
-    currentMax = 0
-    for synset in synsets[token]:
-        if tokenTopScores[token][synset] > currentMax:
-            print(synset.definition(), tokenTopScores[token][synset])
-        currentMax = max(currentMax, tokenTopScores[token][synset])
-    print('')
-
-
-
-
-
+# for token in tokens:
+#     print(token)
+#     currentMax = 0
+#     for synset in synsets[token]:
+#         if tokenTopScores[token][synset] > currentMax:
+#             print(synset.definition(), tokenTopScores[token][synset])
+#         currentMax = max(currentMax, tokenTopScores[token][synset])
+#     print('')
