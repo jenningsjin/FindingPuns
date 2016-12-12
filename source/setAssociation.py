33f58c97105175a81496f7fd9ef1ecdfa15169b0
jenningsjin <jinjenni@umich.edu>
2016-12-11 20:03:26 -0500

    ...

--- source/setAssociation.py
+++ source/setAssociation.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import re
+import json
 import requests
 import itertools
 
@@ -26,41 +27,63 @@ def checkAssocation(word1, word2):
     requestURL = "https://api.projectoxford.ai/text/weblm/v1.0/calculateJointProbability?model=body&order=2"
     authKey = "2a1f5273165e407a8b3981aa640eb565"
     headers = {"Ocp-Apim-Subscription-Key":authKey}
-    # query = "{ \"queries\": [" + word1 + ' ' + word2 + "] }"
+    
     query = """{{\"queries\": [\"{0} {1}\"] }}""".format(word1, word2)
     r = requests.post(requestURL, headers=headers, data=query)
     return r.json()['results'][0]['probability']
 
+#Makes API to Microsoft to figure out scores for an entire List of Bigram
+def checkAssociationBatch(bigramList):
+    requestURL = "https://api.projectoxford.ai/text/weblm/v1.0/calculateJointProbability?model=body&order=2"
+    authKey = "2a1f5273165e407a8b3981aa640eb565"
+    headers = {"Ocp-Apim-Subscription-Key":authKey}
 
+    bigramStrings = [pair[0] + ' ' + pair[1] for pair in  bigramList]
+
+    # query = """{{\"queries\": [\"{0} {1}\"] }}""".format(word1, word2)
+
+
+    query = "{\"queries\":" + str(bigramStrings) + "}"  
+    r = requests.post(requestURL, headers=headers, data=query)
+    
+    outputMap = {} 
+    for data in r.json()['results']:
+        words = data['words'].split(' ')
+        word1 = words[0]
+        word2 = words[1]
+
+        pair = (str(word1), str(word2))
+        if not outputMap.get(pair):
+            outputMap[pair] = float(data['probability'])
+
+    return outputMap
+    # return r.json()['results'][0]['probability']    
 
 
 # Given the Sentence, get the scores for each bigram
 # in the sentence
 def getSentenceScores(sentenceList):
     scoreMap = {}
-    for i in xrange(len(sentenceList)-1):
-        word1 = sentenceList[i]
-        word2 = sentenceList[i+1]
-        score = checkAssocation(word1, word2)
-        bigram = (word1, word2)
+    permutationList = list(itertools.permutations(sentenceList, 2))
+    for bigram in permutationList:
+        word1 = bigram[0]
+        word2 = bigram[1]
 
-        if not scoreMap.get(bigram):
-            scoreMap[bigram] = score
-    return scoreMap
+        print word1 +  ' | ' + word2
+    #     score = checkAssocation(word1, word2)
+    #     bigram = (word1, word2)
+
+    #     if not scoreMap.get(bigram):
+    #         scoreMap[bigram] = score
+    # return scoreMap
+
+
 
 # Find the Word that appears in the highest number of 
 # high scoring sets
-def setCompare(Scores):
+def setCompare(scores):
     return
 
-def processData(filename):
-    return
-
-
-def setCompareFeature(data):
-    for sentence in text:
-        print "hello"
-    return
 
 
 if __name__ == "__main__":
@@ -68,6 +91,8 @@ if __name__ == "__main__":
     dataList = getData(filename)
 
     sentence = "Hello it's me"
-    print getSentenceScores("A dog gave birth to puppies near the road and was cited for littering.".split(' '))
+    # print getSentenceScores("A dog gave birth to puppies near the road and was cited for littering.".split(' '))
+    # print getSentenceScores("A dog gave bircth to puppies".split(' '))
+    listo = [('A', 'dog'), ('A', 'gave'), ('A', 'birth'), ('A', 'to'), ('A', 'puppies'), ('A', 'near'), ('A', 'the')]
 
-
+    print checkAssociationBatch(listo)
